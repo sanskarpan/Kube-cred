b2f6f055716f7d7bd855d19ec68669ee3666756d
sanskarpan <sanskarpandey2004@gmail.com>
2025-10-09 21:06:33 +0530

    Add Jest configuration file for verification service, specifying TypeScript preset, test environment, and coverage settings.

---
+++ services/verification-service/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts'
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        strictNullChecks: false
+      }
+    }
+  }
+};
+
